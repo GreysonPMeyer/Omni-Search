f3bf0fb73837f10f1b98ec6e067f9e9288ee409c
Greyson Meyer <123700168+GreysonPMeyer@users.noreply.github.com>
2024-12-01 14:23:32 -0800

    Changed stuff
    Changed stuff

--- read_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-import requests
-from bs4 import BeautifulSoup
-from concurrent.futures import ThreadPoolExecutor
-
-df = pd.read_csv('/Users/greysonmeyer/Desktop/Erdos_Work/omni_data_cleaned.csv')
-print(df[['artwork_name', 'image_url']].head())
